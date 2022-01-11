908db2a19aa7bf5d7bf7e327cf5f9f5e2130060a
tgldr <rbktgldr@gmail.com>
2022-01-10 21:31:31 -0600

    change to non block

---
+++ assignments/day01/fib2.js
@@ -0,0 +1,4 @@
+var fib = require("./fib");
+
+console.log("Fibonacci of 30: ", fib.calc(30));
+console.log("Fibonacci of -15: ", fib.calc(-15));
--- assignments/day01/index.js
+++ assignments/day01/index.js
@@ -1,4 +1,22 @@
+const child_process = require("child_process");
 var fib = require("./fib");
 
-console.log("Fibonacci of 30:", fib.calc(30));
-console.log("Fibonacci of -15:", fib.calc(-15));
+// using set timeout
+console.log("1: Start");
+
+// async call
+setTimeout(function () {
+  console.log("Fibonacci of 30: ", fib.calc(30));
+  console.log("Fibonacci of -15: ", fib.calc(-15));
+}, 0);
+
+console.log("3: End");
+
+// using child process
+console.log("1: Start");
+
+const newProcess = child_process.spawn("node", ["./fib2.js"], {
+  stdio: "inherit",
+});
+
+console.log("3: End");
